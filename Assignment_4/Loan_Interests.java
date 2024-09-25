561eba88390337601fe5beda58af6b016a22293f
Vaibhav Darji <vaibhav.darji@pc0460.radixweb.local>
2024-09-25 17:15:32 +0530

    Updated Loan_Interests.java exercise

--- Assignment_4/Loan_Interests.java
+++ Assignment_4/Loan_Interests.java
@@ -4,18 +4,26 @@ import java.util.*;
 class Bank{
 	String bank_name;
 	double rate;
+	void getInterestRate() {
+		System.out.format("\nInterest rate at %s is %s%%\n",bank_name,rate);
+	}
 	void displayWithInterest(int amount) {
-			double Interest_Rate_Amount = ((double)amount*rate/100);
-			System.out.format("\nBank name : %s\nLoan Amount : %d\nInterest rate : %.2f\nLoan Interest Amount : %.2f\nTotal payable amount %.2f\n",bank_name,amount,rate ,Interest_Rate_Amount , (amount+Interest_Rate_Amount));
-			System.out.println("-------------------------------------");
-		}
+		System.out.println("This displays the total payable amount");
+	}
+	
 }
 	
 
 class Axis extends Bank{
 	Axis(){
-		bank_name = "Axis";
-		rate=8;
+	bank_name = "Axis";
+	rate=8.0;
+	}
+		
+	void displayWithInterest(int amount) {
+		double Interest_Rate_Amount = ((double)amount*rate/100);
+		System.out.format("\nBank name : %s\nLoan Amount : %d\nInterest rate : %.2f%%\nLoan Interest Amount : %.2f\nTotal payable amount %.2f\n",bank_name,amount,rate ,Interest_Rate_Amount , (amount+Interest_Rate_Amount));
+		System.out.println("-------------------------------------");
 	}
 }
 
@@ -24,6 +32,11 @@ class SBI extends Bank{
 		bank_name = "SBI";
 		rate=7;
 	}
+	void displayWithInterest(int amount) {
+		double Interest_Rate_Amount = ((double)amount*rate/100);
+		System.out.format("\nBank name : %s\nLoan Amount : %d\nInterest rate : %.2f%%\nLoan Interest Amount : %.2f\nTotal payable amount %.2f\n",bank_name,amount,rate ,Interest_Rate_Amount , (amount+Interest_Rate_Amount));
+		System.out.println("-------------------------------------");
+	}
 }
 
 class ICICI extends Bank{
@@ -31,8 +44,12 @@ class ICICI extends Bank{
 		bank_name = "ICICI";
 		rate=9;
 	}
+	void displayWithInterest(int amount) {
+		double Interest_Rate_Amount = ((double)amount*rate/100);
+		System.out.format("\nBank name : %s\nLoan Amount : %d\nInterest rate : %.2f%%\nLoan Interest Amount : %.2f\nTotal payable amount %.2f\n",bank_name,amount,rate ,Interest_Rate_Amount , (amount+Interest_Rate_Amount));
+		System.out.println("-------------------------------------");
+	}
 }
-
 
 
 public class Loan_Interests {
@@ -48,8 +65,13 @@ public class Loan_Interests {
 		SBI sbi = new SBI();
 		ICICI icici = new ICICI();
 		
+		axis.getInterestRate();
 		axis.displayWithInterest(loan_amount);
+		
+		sbi.getInterestRate();
 		sbi.displayWithInterest(loan_amount);
+		
+		icici.getInterestRate();
 		icici.displayWithInterest(loan_amount);
 						
 		

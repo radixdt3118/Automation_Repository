4720819d4f1b79f5a954d2259c3a96d36007b27f
Vaibhav Darji <vaibhav.darji@pc0460.radixweb.local>
2024-10-16 17:18:30 +0530

    Updated customFind.java

--- Assignment_10/TestNGProj/src/main/java/Custom_Wait/Custom_wait.java
+++ Assignment_10/TestNGProj/src/main/java/Custom_Wait/Custom_wait.java
@@ -1,21 +1,9 @@
 package Custom_Wait;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.concurrent.TimeoutException;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Custom_wait {
 
@@ -33,6 +21,7 @@ public class Custom_wait {
 			} catch (Exception e) {
 				// do nothing
 			}
+			count++;
 //			time += interval;
 //			Thread.sleep(interval);			
 		}

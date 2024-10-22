3c26b5d2dadd1da1df1f6ac2ac57addbdc26704c
Vaibhav Darji <vaibhav.darji@pc0460.radixweb.local>
2024-10-22 12:16:03 +0530

    Added groups to suites

--- Assignment_11/TestNGProj/src/test/java/Assignments_Packages/Product_Test.java
+++ Assignment_11/TestNGProj/src/test/java/Assignments_Packages/Product_Test.java
@@ -12,46 +12,48 @@ import Assignments.CRUD_Product;
 
 public class Product_Test {
 
-	@BeforeSuite
+	@BeforeSuite(groups = "WEBDRIVER")
 	public static void invokeAndNavigate() throws IOException, Exception {
 		CRUD_Product.invokeAndNavigate();
 	}
 	
 	
-	@AfterSuite
+	@AfterSuite(alwaysRun = true)
 	public static void quitBrowser() {
+		System.out.println("After suite");
 		CRUD_Product.quitBrowser();
 	}
 	
-	@BeforeClass
+	@BeforeClass(alwaysRun = true)
 	public static void prefetchData() throws Exception {
 		CRUD_Product.preloadData(1);
 	}
 	
-	@BeforeMethod
+	@BeforeMethod(alwaysRun = true)
 	public static void redirectToListing() {
 		CRUD_Product.redirectToListingPage();
 	}
 	
-	@Test(priority = 1,groups = {"CRUD"})
+	@Test(priority = 1,groups = "CRUD")
 	public static void createProduct() throws IOException, Exception {
 		CRUD_Product.addProduct();
 	}
 	
-	@Test(priority = 2,groups = {"CRUD"})
+	@Test(priority = 2,groups = "CRUD")
 	public static void editProduct() throws IOException, Exception {
 		CRUD_Product.editProduct();
 	}
 	
-	@Test(priority = 3,groups = {"CRUD"})
+	@Test(priority = 3,groups = "CRUD")
 	public static void viewProduct() throws IOException, Exception {
 		CRUD_Product.viewProduct();
 	}
 	
 	
-	@Test(dependsOnGroups = {"CRUD"})
+	@Test(priority = 4,groups = "CRUD")
 	public static void deleteProduct() throws IOException, Exception {
 		CRUD_Product.deleteProduct();
 	}
 	
 }
+
